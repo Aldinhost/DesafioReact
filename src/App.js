de690c3707b9a52d76fc9c1b33d6f38d7c8f1551
AldoMardel <aldomardel@gmail.com>
2023-03-22 21:48:32 -0600

    Crear estructura desafio de React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <Routes>
+        {/* 
+          IMPORTANTE:
+            Route necesita 2 props para funcionar
+              path = "direccion de la ruta"
+              element = "componente a renderizar"
+         */}
+          <Route path='/' element = {<h1>Landing Page</h1>} />
+          <Route path='/post/:id' element = {<h1> Articulo  </h1>} />
+          <Route path='/login/' element = {<h1> Login  </h1>} />
+          <Route path='/create/' element = {<h1> Crear Post  </h1>} />
+          
+      </Routes>
+
     </div>
   );
 }
